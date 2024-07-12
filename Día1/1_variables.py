d54d5244e9263e22631baade6605576e0679b229
RenzoArturo2302 <renzoarturo12345@gmail.com>
2024-07-11 21:05:45 -0500

    Variables

---
+++ Día1/1_variables.py
@@ -0,0 +1,10 @@
+# Variable primitivas
+
+nombre = "Eduardo"
+
+edad = 32
+
+soltero = True # Boolean > solamente o es True o es False
+
+nombre = -1 
+# En python los tipos de variables pueden cambiar sin ninguna restricción
